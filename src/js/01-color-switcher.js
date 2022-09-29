57dbb8705fe081052a7969a7a17cfee6c63c1d2f
Yura <104792276+Yura-111@users.noreply.github.com>
2022-09-30 02:22:11 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopStn = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const changeColor = (event) => {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+        event.target.disabled = true;
+    }, 1000);
+}
+
+startBtn.addEventListener('click', changeColor);
+
+
+const stopChangeColor = () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+}
+
+stopStn.addEventListener('click', stopChangeColor);
